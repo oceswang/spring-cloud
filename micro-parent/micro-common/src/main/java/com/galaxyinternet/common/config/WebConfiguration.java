37771c097708ca19a9677ea1fba972d5fcb58b3b
Wang Song <songwang@galaxyintrnet.com>
2017-06-05 14:19:17 +0800

    zuul

--- micro-parent/micro-common/src/main/java/com/galaxyinternet/common/config/WebConfiguration.java
+++ micro-parent/micro-common/src/main/java/com/galaxyinternet/common/config/WebConfiguration.java
@@ -1,10 +1,8 @@
 package com.galaxyinternet.common.config;
 
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-import org.springframework.session.data.redis.config.annotation.web.http.EnableRedisHttpSession;
 
 @EnableDiscoveryClient
-@EnableRedisHttpSession
 public class WebConfiguration {
 
 }
